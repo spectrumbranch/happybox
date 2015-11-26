26f848c6e69e69eb12df50fce68964f84d429eea
dcaruso19 <dcaruso19@gmail.com>
2015-11-26 02:07:58 -0500

    form validation for text description field. must do date next. css color changes to alert box success.

--- static/js/happybox.main.js
+++ static/js/happybox.main.js
@@ -7,6 +7,10 @@ var responseHandler = function() {
 	var response = window.location.search.replace("?","");
 	if (response == "success") {
 		$('#msg').append('<div data-alert class="alert-box success radius">Successfully added memory to the happy box!</div>');
+		$("#msg-box").css("display", "block");
+	}
+	else{	
+		$("#msg-box").css("display", "none");
 	}
 };
 
@@ -19,12 +23,17 @@ var setupGetHappyButton=function() {
 			var msgDate = new Date(data.date);
 
 			$("#msg-date").text((msgDate.getMonth() + 1) + "/" + msgDate.getDate() + "/" + msgDate.getFullYear());
+			$("#msg-box").css("display", "block");
 
 		});
 
-
-
-
-
 	});
+}
+
+var validateForm=function() {
+    var x = $("#text-description").val();
+    if (x == null || x == "") {
+        $("#alert1").text("Name must be filled out");
+        return false;
+    }
 }
